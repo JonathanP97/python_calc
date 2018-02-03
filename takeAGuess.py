9d25703a7d38f3dba7a3c0f4673482ad47e84428
Jonathan <jonathan_plata@ymail.com>
2018-02-03 13:34:38 -0800

    guessing game

---
+++ takeAGuess.py
@@ -0,0 +1,23 @@
+from random import randint
+
+goal = randint(1, 99)
+user_guess = 0
+turns = 0
+
+print("\nHi! Lets play a game\nI'm thinking of a number between 1 - 99\n")
+
+# print( randint(1, 99))
+# print(goal)
+
+
+while goal != user_guess:
+	user_guess = int(input('\n'))
+	turns +=1
+	
+	if user_guess < goal:
+		print('\nNope, try higher')
+	elif user_guess > goal:
+		print('\nNope, try less')
+	if user_guess == goal:
+		print('\nwoaaaaah, way to go')
+		print('\nYou did that in {t} turn(s)'.format(t = turns))
